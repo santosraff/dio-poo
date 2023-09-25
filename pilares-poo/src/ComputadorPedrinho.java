fbe50990ef0d1bc973ef669f527b1d9ca5d59788
Rafael Santos <rafoliael@gmail.com>
2023-09-25 18:45:46 -0300

    feat: inicio do exemplo dos pilares

---
+++ pilares-poo/src/ComputadorPedrinho.java
@@ -0,0 +1,15 @@
+/*
+ * Simulação do uso da classe MSNMessenger
+ */
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        // abrindo MSN Messenger
+        MSNMessenger msn = new MSNMessenger();
+
+        msn.validarConectadoInternet();
+        msn.enviarMensagem();
+        msn.salvarHistoricoMensagem();
+
+        msn.receberMensagem();
+    }
+}
