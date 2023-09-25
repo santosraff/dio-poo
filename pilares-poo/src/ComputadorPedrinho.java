7852ac405bec317d8e0bdce35f8dc50be751d209
Rafael Santos <rafoliael@gmail.com>
2023-09-25 18:46:40 -0300

    feat: exemplo de encapsulamento

--- pilares-poo/src/ComputadorPedrinho.java
+++ pilares-poo/src/ComputadorPedrinho.java
@@ -6,9 +6,7 @@ public class ComputadorPedrinho {
         // abrindo MSN Messenger
         MSNMessenger msn = new MSNMessenger();
 
-        msn.validarConectadoInternet();
         msn.enviarMensagem();
-        msn.salvarHistoricoMensagem();
 
         msn.receberMensagem();
     }
