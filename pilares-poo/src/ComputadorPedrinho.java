0c849057bfce2978967b56dfb729ea673229a6e2
Rafael Santos <rafoliael@gmail.com>
2023-09-25 19:07:58 -0300

    feat: exemplo de herança

--- pilares-poo/src/ComputadorPedrinho.java
+++ pilares-poo/src/ComputadorPedrinho.java
@@ -1,13 +1,15 @@
-/*
- * Simulação do uso da classe MSNMessenger
- */
 public class ComputadorPedrinho {
     public static void main(String[] args) {
-        // abrindo MSN Messenger
         MsnMessenger msn = new MsnMessenger();
-
         msn.enviarMensagem();
-
         msn.receberMensagem();
+
+        FacebookMessenger fbm = new FacebookMessenger();
+        fbm.enviarMensagem();
+        fbm.receberMensagem();
+
+        Telegram tlg = new Telegram();
+        tlg.enviarMensagem();
+        tlg.receberMensagem();
     }
 }
--- pilares-poo/src/MSNMessenger.java
+++ pilares-poo/src/MSNMessenger.java
@@ -1,20 +1,3 @@
-public class MsnMessenger {
-    public void enviarMensagem() {
-        validarConectadoInternet();
-        System.out.println("Enviando mensagem");
-        salvarHistoricoMensagem();
+public class MsnMessenger extends ServicoMensagemInstantanea{
 
-    }
-
-    public void receberMensagem() {
-        System.out.println("Recebendo mensagem");
-    }
-
-    private void validarConectadoInternet() {
-        System.out.println("Validando se está conectado a internet");
-    }
-
-    private void salvarHistoricoMensagem() {
-        System.out.println("Salvando o histórico da mensagem");
-    }
 }
